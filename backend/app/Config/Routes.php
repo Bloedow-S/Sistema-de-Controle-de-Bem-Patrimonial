290af2bed5defab0649770f90a4fc19c87d2ec3c
Sabrina Bloedow <sabrinajobsilva@gmail.com>
2026-09-02 16:04:08 -0300

    feat: adiciona rota e finaliza estrutura para teste de api no login

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -6,4 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('api/teste', 'Api::teste');
+
+$routes->get('api/teste', 'Api::teste'); //Api -> Api.php; teste -> método dentro do arquivo.
+
+$routes->get('api/login', 'AuthController::Index'); /*O react que aparece com essa uri "api/login",
+Declaramos ela para o back poder fazer algo (ação) quando receber a requisição + URI*/
+ 
--- backend/app/Controllers/AuthController.php
+++ backend/app/Controllers/AuthController.php
@@ -8,7 +8,7 @@ use Firebase\JWT\JWT;
 
 class AuthController extends ResourceController
 {
-    public function index()
+    public function index() //esse método tem que retornar algo: sts legado -> view; projeto -> rota 
     {
         $usuarioModel = new UsuariosModel();
 
@@ -16,20 +16,26 @@ class AuthController extends ResourceController
         $senha = $this->request->getPost("senha");
 
         $usuario = $usuarioModel->where('email',$email)->first();
-        //Bcrypt: password_verify() extrai o salt da senha armazenada, atribui à recebida e recalcula automaticamente; 
+
+        //Bcrypt: password_verify() extrai o salt da senha armazenada, atribui à recebida e recalcula automaticamente para comparar; 
+        //payload: é o corpo do token -> a seção central
         if ($usuario && password_verify($senha, $usuario->senha)) {
-            $payload = [
-                'iat' => time(),
-                'exp' => time() + 3600,
-                'id' => $usuario['id'],
-                'papel' => $usuario['papel'],
+            $payload = [        
+                'iat' => time(),                //hora da criação do token -> time() pega o horário atual em formato Unix Timestamp
+                'exp' => time() + 3600,         //Define a hora limite de validade do token
+                'id' => $usuario['id'],         
+                'papel' => $usuario['papel'],    //id e papel identificam a permissão de cada requisição solicitada, são enviadas ao back.   
             ];
 
             $chave = getenv('JWT_SECRET');
             $token = JWT::encode($payload, $chave, 'HS256'); //algorítmo de assinatura = HS256
+
+            return $this->respond([     //respond (método do Resource) -> converte o array pra JSON antes de mandar pro cliente
+                'mensagem' => 'Login realizado com sucesso.',
+                'token' => $token,
+            ]);            
         } else {
             return $this->failUnauthorized('Credenciais inválidas');
         }
-
     }
 }
